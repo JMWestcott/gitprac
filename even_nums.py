96e2fa9f4d5774d8891b1ad79f20e90aaefcf41f
JMWestcott <jill.m.westcott@gmail.com>
2018-07-19 17:39:32 -0400

    Updated.

--- even_nums.py
+++ even_nums.py
@@ -1,5 +1,5 @@
 results = []
-for num in range(20):
+for num in range(10):
     if num % 2 == 0:
         results.append(num)
-print("List of even numbers between 0 and 20:", results)
+print("List of even numbers between 0 and 10:", results)
